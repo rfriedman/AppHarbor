f49f1cefaa420ad1ff64a7e9ed574f4fc9dd8c24
gngrbrd <friedman.richard@gmail.com>
2014-12-15 22:27:22 -0500

    initial commit for test

---
+++ AppHarborTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppHarborTest.Startup))]
+namespace AppHarborTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
